20d1853c9fdae7e1bb277ce73b240662bd458416
Ashley Camba Garrido <ashwoods@gmail.com>
2011-08-01 17:01:01 +0200

    fixed function name

--- postgresql/manifests/dbsuper.pp
+++ postgresql/manifests/dbsuper.pp
@@ -1,7 +1,7 @@
 # Quick implementation with database creation and superuser role grants
 # Requires a db name ($name) and a role. To be adapted for specific cases
 #
-define postgresql::dbcreate ($role , $password='' , $conntype="host" , $address='127.0.0.1/32' , $auth_method="md5" , $auth_options ="") {
+define postgresql::dbsuper ($role , $password='' , $conntype="host" , $address='127.0.0.1/32' , $auth_method="md5" , $auth_options ="") {
     exec { "role_$name":
         user    => "${postgresql::params::processuser}",
         unless  => "echo \\\\dg | psql | grep $name 2>/dev/null",
